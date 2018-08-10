373b4cd6ef1cebc6d3edff29bd7d82c8fa6350e3
Alican Çubukçuoğlu <alicancubukcuoglu@gmail.com>
2018-08-10 11:40:58 +0300

    [FfmpegHelper] Cleanup

--- src/FfmpegHelper.js
+++ src/FfmpegHelper.js
@@ -1,7 +1,22 @@
 // @flow
 
-import child_process from 'child_process';
+import child_process, { type ChildProcess } from 'child_process';
 import fse from 'fs-extra';
+
+function promiseFromChildProcess(childProcess: ChildProcess): Promise<void> {
+  return new Promise((resolve, reject) => {
+    childProcess.on('error', (error) => {
+      reject(error);
+    });
+    childProcess.on('exit', (code) => {
+      if (code !== 0) {
+        reject(new Error(`Process exited with "${code}"`));
+      }
+
+      resolve();
+    });
+  });
+}
 
 export default class FfmpegHelper {
   command: string;
@@ -10,29 +25,26 @@ export default class FfmpegHelper {
     this.command = command;
   }
 
-  async transcodeVideo(inputPath: string, outputPath: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-      child_process.exec(
-        this.command +
-          ' -i ' +
-          inputPath +
-          ' -vf setdar=16/9 -video_track_timescale 60000 -ac 1 -ar 48000 -preset ultrafast ' +
-          // '-vf "[in]drawtext=fontfile=misc/segoeuil.ttf: text=\'twitch.tv/' + parser.clipList[index].broadcaster.name +
-          // '\': fontcolor=black: fontsize=20: box=1: boxcolor=white@0.9: boxborderw=10: x=(w-text_w)-10: y=40,' +
-          // 'drawtext=fontfile=misc/segoeuil.ttf: text=' + parser.clipList[index].game.replace(':', '') +
-          // '\': fontcolor=black: fontsize=20: box=1: boxcolor=white@0.9: boxborderw=10: x=(w-text_w)-10: y=100" ' +
-          outputPath +
-          ' -y',
-        (error, stdout, stderr) => {
-          if (error) {
-            reject(error);
-            return;
-          }
+  spawn(args: Array<string>, options?: child_process$spawnOpts): ChildProcess {
+    return child_process.spawn(this.command, args, options);
+  }
 
-          resolve();
-        },
-      );
-    });
+  async transcodeVideo(inputPath: string, outputPath: string): Promise<void> {
+    const ffmpeg = this.spawn(
+      // prettier-ignore
+      [
+        '-i', inputPath,
+        '-vf', 'setdar=16/9',
+        '-video_track_timescale', '60000',
+        '-ac', '1',
+        '-ar', '48000',
+        '-preset', 'ultrafast',
+        outputPath,
+        '-y',
+      ],
+    );
+
+    return promiseFromChildProcess(ffmpeg);
   }
 
   async concatVideos(
@@ -44,17 +56,18 @@ export default class FfmpegHelper {
 
     await fse.writeFile(listPath, videoPaths.map((f) => `file '${f}'`).join('\n'));
 
-    return new Promise((resolve, reject) => {
-      child_process.exec(
-        this.command + ' -f concat -safe 0 -i ' + listPath + ' -c copy ' + outputPath + ' -y',
-        (error, stdout, stderr) => {
-          if (error) {
-            reject(error);
-            return;
-          }
-          resolve();
-        },
-      );
-    });
+    const ffmpeg = this.spawn(
+      // prettier-ignore
+      [
+        '-f', 'concat',
+        '-safe', '0',
+        '-i', listPath,
+        '-c', 'copy',
+        outputPath,
+        '-y'
+      ],
+    );
+
+    return promiseFromChildProcess(ffmpeg);
   }
 }
