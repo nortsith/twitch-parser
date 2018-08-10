c381b38c95e99f8dad4394d51adf17ad961081ab
Alican Çubukçuoğlu <alicancubukcuoglu@gmail.com>
2018-08-10 14:17:59 +0300

    Use "node-fetch" instead of "download-file"

--- src/downloadFile.js
+++ src/downloadFile.js
@@ -1,23 +1,18 @@
 // @flow
 
 import path from 'path';
-import download from 'download-file';
+import fs from 'fs';
+import fetch from 'node-fetch';
 
-export default function downloadFile(url: string, outputPath: string): Promise<void> {
-  const { dir, base } = path.parse(outputPath);
+export default async function downloadFile(url: string, outputPath: string): Promise<void> {
+  const writeStream = fs.createWriteStream(outputPath);
 
-  const options = {
-    directory: dir,
-    filename: base,
-  };
+  const response = await fetch(url);
+
+  response.body.pipe(writeStream);
 
   return new Promise((resolve, reject) => {
-    download(url, options, (error) => {
-      if (error) {
-        reject(error);
-        return;
-      }
-      resolve();
-    });
+    writeStream.on('error', (error) => reject(error));
+    writeStream.on('close', () => resolve());
   });
 }
